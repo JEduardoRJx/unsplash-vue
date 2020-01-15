5812d0f22d4cab91a8266e298315e5aa28d10641
Eduardo <jeduardorjx@gmail.com>
2020-01-14 22:45:58 -0700

    Create apicall for a random photo

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,11 @@
 /* eslint-disable no-console */
 // const accKey = 'ab3027cec37ea90bdc0cd87ba2a9a21724432ed78fce635c435f9f67f16bbbca'
-const accKey2 = 'aef461ed8867166f80a247e0f33bc7131aee02e22ca16c434933b579284bd9dd'
+const accKey2 = 'aef461ed8867166f80a247e0f33bc7131aee02e22ca16c434933b579284bd9dd';
+
+export const randomPhoto = async () => {
+  const response = await fetch(`https://api.unsplash.com/photos/random/?client_id=${accKey2}`)
+  if(!response.ok) {
+    throw Error ("Couldn't get random image")
+  }
+  return response.json();
+}
