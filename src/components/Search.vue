8db90e3b805149e64d035f16cce72dd9f6a9ec8a
Eduardo <jeduardorjx@gmail.com>
2020-01-14 22:37:51 -0700

    Skeleton for search component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,19 @@
 <template>
   <section>
-
+    <div>
+      <form>
+        <button>
+          <img />
+        </button>
+        <input />
+        <button>
+          <img />
+        </button>
+      </form>
+    </div>
+    <div>
+      <h1>searchTerm Here</h1>
+    </div>
   </section>
 </template>
 
