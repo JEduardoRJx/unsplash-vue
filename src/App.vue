3fe2ad4d8bf381e28872ed1929850e59fffbbf98
Eduardo <jeduardorjx@gmail.com>
2020-01-14 22:25:06 -0700

    Import search and imgcontainer components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
   <main id="app">
-
+    <Search />
+    <ImgContainer />
   </main>
 </template>
 
 <script>
+import Search from './components/Search'
+import ImgContainer from './components/ImgContainer';
+
+
 export default {
-  
+  name: 'app',
+  components: {
+    Search,
+    ImgContainer,
+  }
 }
 </script>
 
 <style scoped>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
 
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+  }
 </style>
