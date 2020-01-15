89506ba91ad8add735c7d50636fea6906b854bef
Eduardo <jeduardorjx@gmail.com>
2020-01-14 22:45:30 -0700

    Import unsplash access code

---
+++ src/apiCalls.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-console */
+// const accKey = 'ab3027cec37ea90bdc0cd87ba2a9a21724432ed78fce635c435f9f67f16bbbca'
+const accKey2 = 'aef461ed8867166f80a247e0f33bc7131aee02e22ca16c434933b579284bd9dd'
