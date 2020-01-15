1ef0afe63c4c092ec3b27a000bebb0498de6ecdd
Eduardo <jeduardorjx@gmail.com>
2020-01-14 22:13:14 -0700

    Set the rest of my components

---
+++ src/components/ImgCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <a>
+    
+  </a>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ImgContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class='img-container'>
+
+  </section>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
