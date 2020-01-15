a3b2f704b8d81da918f209d608b99dd90853a53a
Eduardo <jeduardorjx@gmail.com>
2020-01-14 23:15:32 -0700

    Crate api for querying images

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,3 +9,11 @@ export const randomPhoto = async () => {
   }
   return response.json();
 }
+
+export const searchImages = async (query) => {
+  const response = await fetch(`https://api.unsplash.com/search/photos?query=${query}&client_id=${accKey2}`)
+  if(!response.ok) {
+    throw Error ("No images match search term, search again.");
+  }
+  return response.json();
+}
